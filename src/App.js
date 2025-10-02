c5815a850f450cfe2dadb1b0695800ffa0fcaf03
Could I be a spy? <collethom@gmail.com>
2025-10-02 17:18:41 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,11 +3,26 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function Home() {
-  return <h2>Home Page</h2>;
+  return <body>
+    <h2>Home Page</h2>
+    <p>Welcome on the first version of Artooro</p>
+    </body>;
 }
 
 function About() {
-  return <h2>About Page</h2>;
+  return <body>
+    <h2>About Page</h2>
+      <p>Artooro was created as a guide, to let you sink into your next city trips, and have a deeper taste of the city's culture.</p>
+      <p>We are also fond of learning new languages so we might have add some of these elements inside.</p>
+      <p>Hope you will enjoy our work. Cheers!</p>
+  </body>
+ 
+}
+
+function Start() {
+  return <body>
+    <h2>Let's start the TOOR!</h2>
+    </body>
 }
 
 function App() {
@@ -16,10 +31,12 @@ function App() {
       <nav>
         <Link to="/">Home</Link> |{' '}
         <Link to="/about">About</Link>
+        <Link to="/start">Start</Link>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/start" element={<Start />} />
       </Routes>
     </Router>
   );
